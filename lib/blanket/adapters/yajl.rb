8d0103b71531f54878eef4f7fcf55d2368dfe3a2
Ari Summer <aribsummer@gmail.com>
2015-03-21 21:23:16 -0700

    Add post, put, and delete methods to yajl adapter

--- lib/blanket/adapters/yajl.rb
+++ lib/blanket/adapters/yajl.rb
@@ -10,6 +10,24 @@ module Blanket
         ::Yajl::HttpStream.get uri, generate_options(options), &block
       end
 
+      def self.post(uri, options = {}, &block)
+        query = query_string options[:params]
+        uri = "#{uri}?#{query}" unless query.empty?
+        ::Yajl::HttpStream.post uri, options[:body], generate_options(options), &block
+      end
+
+      def self.put(uri, options = {}, &block)
+        query = query_string options[:params]
+        uri = "#{uri}?#{query}" unless query.empty?
+        ::Yajl::HttpStream.put uri, options[:body], generate_options(options), &block
+      end
+
+      def self.delete(uri, options = {}, &block)
+        query = query_string options[:params]
+        uri = "#{uri}?#{query}" unless query.empty?
+        ::Yajl::HttpStream.delete uri, generate_options(options), &block
+      end
+
       private
       def self.parse_response(hash)
         [hash].flatten.map { |item| RecursiveOpenStruct.new item, recurse_over_arrays: true }
