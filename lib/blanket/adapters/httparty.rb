86dee160b28f2bfcd09094671a2081d18af44965
Ari Summer <aribsummer@gmail.com>
2015-03-21 21:23:31 -0700

    Add post, put, and delete methods to httparty adapter

--- lib/blanket/adapters/httparty.rb
+++ lib/blanket/adapters/httparty.rb
@@ -8,6 +8,21 @@ module Blanket
         response.to_hash
       end
 
+      def self.post(uri, options = {}, &block)
+        response = ::HTTParty.post uri, generate_options(options), &block
+        response.to_hash
+      end
+
+      def self.put(uri, options = {}, &block)
+        response = ::HTTParty.put uri, generate_options(options), &block
+        response.to_hash
+      end
+
+      def self.delete(uri, options = {}, &block)
+        response = ::HTTParty.delete uri, generate_options(options), &block
+        response.to_hash
+      end
+
       private
       def self.generate_options(options = {})
         new_options = options.dup
