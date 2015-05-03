467f04aa2ca817717d6bf2eec54ce098cdf9912d
Ari Summer <aribsummer@gmail.com>
2015-05-03 11:36:36 -0700

    Update Local class for testing

--- lib/test/local.rb
+++ lib/test/local.rb
@@ -2,10 +2,16 @@ require 'blanket/wrap'
 require 'pry'
 
 Dir["#{File.dirname(__FILE__)}/representers/**/*.rb"].each { |f| load(f) }
-Dir["#{File.dirname(__FILE__)}/jobs/**/*.rb"].each { |f| load(f) }
+# Dir["#{File.dirname(__FILE__)}/jobs/**/*.rb"].each { |f| load(f) }
 
 module Blanket
   module Test
+    class Foo
+      include Wrap
+
+      uri 'http://localhost:3000/v1/groups'
+    end
+
     class Local
       include Wrap
 
@@ -14,19 +20,18 @@ module Blanket
       # param :per_page, 1
       # extension :json
       # adapter :yajl
-      before_request :beforehand, actions: [:get]
-      after_request :afterwards
+      before_request :beforehand, actions: [:get], path: //
+      after_request :afterwards, path: //
 
-      # Default path is ''
+      # Default path is /^$/
       # Default actions are all actions
 
-      # representer V1::GroupRepresenter, path: '/v1/groups'
+      # representer V1::GroupRepresenter, path: /\/v1\/groups/
       # background V1::GroupsJob, path: '/v1/groups'
       # before_request :some_method, path: '/v1/groups', only: [:get]
 
-      # Instantiate uri with base_uri + path when instantiating/delegating
       # Delegate to class when performing http action
-      # delegate V1::Groups, path: [:v1, :groups]
+      # delegate_action :get, to: Foo, path: //, options: {  params: { per_page: 1 } }
 
       def beforehand(action)
         puts 'this is before'
